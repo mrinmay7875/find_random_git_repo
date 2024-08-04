3681a5ecd9042d5145a40b55669cc39a284ad1cc
Mrinmay Santra <mrinmay7875@gmail.com>
2024-08-05 01:00:36 +0530

    Added Alert component for rate limit error

--- src/components/RepositoryCard.tsx
+++ src/components/RepositoryCard.tsx
@@ -1,6 +1,6 @@
-import { Anchor, Badge, Card, Group, Image, Text } from '@mantine/core';
-import React from 'react';
-import { Alert } from '@mantine/core';
+import React, { useState } from 'react';
+import { Anchor, Badge, Card, Group, Text, Alert } from '@mantine/core';
+
 import { IconInfoCircle } from '@tabler/icons-react';
 
 export type RepositoryCardProps = {
@@ -22,58 +22,68 @@ function RepositoryCard({
 }: RepositoryCardProps) {
   const icon = <IconInfoCircle />;
 
-  if (isError) {
+  const [isAlertOpen, setIsAlertOpen] = useState(isError);
+
+  // Hides the Error Alert component
+  function handleCloseAlert() {
+    console.log('alert closed');
+    setIsAlertOpen(false);
+  }
+
+  if (isError && isAlertOpen) {
+    return (
+      <Group mt='md' mb={15} justify='center'>
+        <Alert
+          variant='outline'
+          color='red'
+          withCloseButton
+          title='Error'
+          icon={icon}
+          onClose={handleCloseAlert}
+        >
+          You made too many requests. Please wait for around 15 seconds and try
+          again.
+        </Alert>
+      </Group>
+    );
+  }
+
+  if (!isError && !isAlertOpen) {
     return (
       <div>
         <Group mt='md' mb={15} justify='center'>
-          <Alert
-            variant='outline'
-            color='red'
-            withCloseButton
-            title='Error'
-            icon={icon}
+          <Card
+            shadow='sm'
+            padding='xl'
+            component='div'
+            style={{ width: 500 }}
+            withBorder
           >
-            Something went wrong. Please try again after sometime.
-          </Alert>
+            <Anchor href={repoURL} target='_blank'>
+              {name}
+            </Anchor>
+            <Text mt='xs' c='dimmed' size='sm' component='div'>
+              <strong>Description:</strong>
+              {description}
+            </Text>
+            <Text mt='xs' size='sm' component='div'>
+              <strong>⭐️ Stars:</strong>
+              <Text c='dimmed' size='lg' component='div'>
+                {stars}
+              </Text>
+            </Text>
+            <div>
+              {topics.map((topic, index) => (
+                <Badge key={index} color='blue' mr={10}>
+                  {topic}
+                </Badge>
+              ))}
+            </div>
+          </Card>
         </Group>
       </div>
     );
   }
-
-  return (
-    <div>
-      <Group mt='md' mb={15} justify='center'>
-        <Card
-          shadow='sm'
-          padding='xl'
-          component='div'
-          style={{ width: 500 }}
-          withBorder
-        >
-          <Anchor href={repoURL} target='_blank'>
-            {name}
-          </Anchor>
-          <Text mt='xs' c='dimmed' size='sm' component='div'>
-            <strong>Description:</strong>
-            {description}
-          </Text>
-          <Text mt='xs' size='sm' component='div'>
-            <strong>⭐️ Stars:</strong>
-            <Text c='dimmed' size='lg' component='div'>
-              {stars}
-            </Text>
-          </Text>
-          <div>
-            {topics.map((topic, index) => (
-              <Badge key={index} color='blue' mr={10}>
-                {topic}
-              </Badge>
-            ))}
-          </div>
-        </Card>
-      </Group>
-    </div>
-  );
 }
 
 export default RepositoryCard;
